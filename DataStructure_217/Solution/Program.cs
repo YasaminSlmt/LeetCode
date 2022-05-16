5f2d270c1d078c2b085eaa9a3f8599a02b106e0a
Yasamin Salamat <y.salamat@bymmt.com>
2022-05-15 22:28:54 -0400

    Add project files.

---
+++ DataStructure_217/Solution/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1,2,3,4};
+            var numsCheck = new Dictionary<int,int>();
+            bool results = new bool();
+            results = false;
+            
+            
+            // Using sort: Higher running time, less memory utilization
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    results = true;
+                    break;
+
+                }
+                
+            }
+
+            // Using dictionary: Less runtime, higher memory consumption
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (numsCheck.ContainsKey(nums[i]))
+            //    {
+            //        results = true;
+            //        break;
+
+            //    }
+            //    else
+            //        numsCheck[nums[i]] = 1;
+
+
+
+            //    var numsCheck = new Dictionary<int, int>();
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (numsCheck.ContainsKey(nums[i]))
+            //            return true;
+            //        else
+            //            numsCheck[nums[i]] = 1;
+            //    }
+
+            //    return false;
+            Console.WriteLine(results);
+
+        }
+
+            
+            
+
+    }
+}
---
+++ DataStructure_53/Solution/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        
+        static int maxSubArraysCrossMid (int[] nums, int startIdx, int endIdx, int midIdx)
+        {
+            int sum = 0;
+
+            int leftSum = int.MinValue;
+            for (int i = midIdx; i >= startIdx; i--)
+            {
+                sum += nums[i];
+                if (sum > leftSum)
+                    leftSum = sum;
+            }
+
+            sum = 0;
+            int rightSum = int.MinValue;
+            for (int i = midIdx+1; i <= endIdx; i++)
+            {
+                sum += nums[i];
+                if (sum > rightSum)
+                    rightSum = sum;
+            }
+
+            return Math.Max(rightSum+leftSum, Math.Max(rightSum, leftSum));
+        }
+        
+        
+        static int maxSubArraySum(int[] nums, int startIdx, int endIdx)
+        {
+            if (startIdx == endIdx)
+                return nums[startIdx];
+            else
+            {
+                int midIdx = 0;
+
+                // Finding the index of the middle element
+                midIdx = (endIdx + startIdx) / 2;
+
+
+                return Math.Max(Math.Max(maxSubArraySum(nums, startIdx, midIdx), maxSubArraySum(nums, midIdx + 1, endIdx)),maxSubArraysCrossMid(nums, startIdx,endIdx,midIdx));
+
+
+
+
+            }
+            
+           
+        }
+        
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 2, 3, 4, 5, 7 };
+
+
+            Console.WriteLine(maxSubArraySum(nums, 0, nums.Length - 1));
+        }
+    }
+}
